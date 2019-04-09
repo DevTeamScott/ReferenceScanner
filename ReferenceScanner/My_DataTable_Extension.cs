91421036226d6cc1adbefe758246945f8c5e9a5b
DevTeamScott <49155702+DevTeamScott@users.noreply.github.com>
2019-04-09 12:52:44 +0100

    Reference Scanner Class added

---
+++ ReferenceScanner/My_DataTable_Extension.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+
+
+namespace ReferenceScanner
+{
+    public static class My_DataTable_Extensions
+    {
+        /// <summary>
+        /// This most excellent chap takes a datatable and dumps out an Excel spreadsheet.
+        /// 
+        /// Extension to the datatable method.
+        /// </summary>
+        /// <param name="DataTable">Provide a DataTable</param>
+        /// <param name="excelFilePath">Provide a filepath to file.</param>
+        public static void ExportToExcel(this System.Data.DataTable DataTable, string fileName, string excelFilePath = null)
+        {
+            try
+            {
+                int ColumnsCount;
+
+                if (DataTable == null || (ColumnsCount = DataTable.Columns.Count) == 0)
+                    throw new Exception("ExportToExcel: Null or empty input table!\n");
+
+                // loading excel and creating a new workbook
+                Microsoft.Office.Interop.Excel.Application Excel = new Microsoft.Office.Interop.Excel.Application();
+                Excel.Workbooks.Add();
+
+                // creat a single worksheet
+                Microsoft.Office.Interop.Excel._Worksheet Worksheet = Excel.ActiveSheet;
+
+                object[] Header = new object[ColumnsCount];
+
+                // loop through column headings
+                for (int i = 0; i < ColumnsCount; i++)
+                {
+                    Header[i] = DataTable.Columns[i].ColumnName;
+                }
+
+
+                Microsoft.Office.Interop.Excel.Range HeaderRange = Worksheet.get_Range((Microsoft.Office.Interop.Excel.Range)
+                    (Worksheet.Cells[1, 1]), (Microsoft.Office.Interop.Excel.Range)(Worksheet.Cells[1, ColumnsCount]));
+                HeaderRange.Value = Header;
+                HeaderRange.Font.Bold = true;
+
+                // DataCells
+                int RowsCount = DataTable.Rows.Count;
+                object[,] Cells = new object[RowsCount, ColumnsCount];
+
+                for (int j = 0; j < RowsCount; j++)
+                {
+                    for (int i = 0; i < ColumnsCount; i++)
+                    {
+                        Cells[j, i] = DataTable.Rows[j][i];
+                    }
+                }
+
+                Worksheet.get_Range((Microsoft.Office.Interop.Excel.Range)(Worksheet.Cells[2, 1]),
+                    (Microsoft.Office.Interop.Excel.Range)(Worksheet.Cells[RowsCount + 1, ColumnsCount + 1])).Value = Cells;
+
+                // for file path locations
+                if (excelFilePath != null && excelFilePath != "")
+                {
+                    try
+                    {
+                        Worksheet.SaveAs(excelFilePath + "cleaned_" + fileName);
+                        Excel.Quit();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        throw new Exception("ExportToExcel: Excel file could not be saved! check file path.\n"
+                            + ex.Message);
+                    }
+                }
+                else
+                {
+                    Excel.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("ExportToExcel : \n" + ex.Message);
+            }
+        }
+    }
+}
---
+++ ReferenceScanner/ReferenceCheckedData.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace ReferenceScanner
+{
+
+    public enum FileType { xls, xlsx, xlsm, csv};
+    public enum OutputFormat { Excel, CSV, DataTable};
+    
+    public class ReferenceCheckedData
+    {
+
+#region Private Methods
+
+        ///// <summary>
+        ///// Operates on every row and column in the supplied data source.
+        ///// </summary>
+        ///// <param name="filePath">FilePath to where the excel or csv file is located.</param>
+        ///// <param name="outputFormat">What data structure the output should be.</param>
+        ///// <param name="inputExtention">File extension of the input file.</param>
+        ///// <param name="sheetName">Optional Param for the sheet name if input is of type Excel</param>
+        ///// <returns></returns>
+        //private DataTable OutputScannedData(string filePath, OutputFormat outputFormat, FileType inputExtention, string sheetName = "Sheet1")
+        //{
+        //    string strFileName = filePath;
+        //    New_Wrapper.DataHandler myHandler = new New_Wrapper.DataHandler();
+
+        //    string fileLocation = Path.GetDirectoryName(strFileName);
+
+        //    DataTable cleanedData = new DataTable();
+
+        //    if (inputExtention != FileType.csv)
+        //    {
+        //        cleanedData = myHandler.ReturnExcelSheetAsDataTable(strFileName, sheetName, true);
+        //    }
+        //    else
+        //    {
+        //        cleanedData = myHandler.ReturnCSVAsDataTable(strFileName, true);
+        //    }
+
+        //    if (cleanedData.Columns.Count > 11)
+        //    {
+        //        int i = 0;
+        //        for (int j = cleanedData.Columns.Count - 1; j >= 10; j--)
+        //        {
+        //            if (cleanedData.Columns[j].ColumnName.Contains(Path.GetFileNameWithoutExtension(strFileName)))
+        //            {
+        //                cleanedData.Columns.RemoveAt(j);
+        //            }
+        //            i++;
+        //        }
+        //    }
+
+        //    cleanedData.Columns.Add("NewReference", typeof(string));
+        //    cleanedData.Columns.Add("HOD", typeof(string));
+
+        //    ReferenceParser.ReferenceParser rp = new ReferenceParser.ReferenceParser();
+
+        //    foreach (DataRow dr in cleanedData.Rows)
+        //    {
+        //        bool found = false;
+        //        foreach (DataColumn dc in cleanedData.Columns)
+        //        {
+
+        //            if (found == false)
+        //            {
+        //                dr["NewReference"] = "";
+        //                dr["HOD"] = "";
+        //                foreach (string s in rp.Parse(dr[dc.ColumnName].ToString(), true, true))
+        //                {
+
+        //                    if (s.Trim() != "")
+        //                    {
+        //                        string[] splitRefHod = s.Split(new string[] { "??$??" }, StringSplitOptions.None);
+        //                        dr["NewReference"] = splitRefHod[0];
+        //                        dr["HOD"] = splitRefHod[1];
+        //                        found = true;
+        //                        break;
+        //                    }
+        //                    else
+        //                    {
+        //                        found = false;
+        //                        dr["NewReference"] = "";
+        //                        dr["HOD"] = "";
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return cleanedData;
+        //}
+
+
+        /// <summary>
+        /// Overload - Operatate on defined columns provided by a list.
+        /// </summary>
+        /// <param name="filePath">FilePath to where the excel or csv file is located.</param>
+        /// <param name="outputFormat">What data structure the output should be.</param>
+        /// <param name="inputExtention">File extension of the input file.</param>
+        /// <param name="columnHeaders">List of columns headers to parse for references.</param>
+        /// <param name="sheetName">Optional Param for the sheet name if input is of type Excel</param>
+        /// <returns>Datatable</returns>
+        private DataTable OutputScannedData(string filePath, OutputFormat outputFormat, FileType inputExtention, List<string> columnHeaders, string sheetName = "Sheet1")
+        {
+            string strFileName = filePath;
+            New_Wrapper.DataHandler myHandler = new New_Wrapper.DataHandler();
+
+            string fileLocation = Path.GetDirectoryName(strFileName);
+
+            DataTable cleanedData = new DataTable();
+
+            if (inputExtention != FileType.csv)
+            {
+                cleanedData = myHandler.ReturnExcelSheetAsDataTable(strFileName, sheetName, true);
+            }
+            else
+            {
+                cleanedData = myHandler.ReturnCSVAsDataTable(strFileName, true);
+            }
+
+            if (cleanedData.Columns.Count > 11)
+            {
+                int i = 0;
+                for (int j = cleanedData.Columns.Count - 1; j >= 10; j--)
+                {
+                    if (cleanedData.Columns[j].ColumnName.Contains(Path.GetFileNameWithoutExtension(strFileName)))
+                    {
+                        cleanedData.Columns.RemoveAt(j);
+                    }
+                    i++;
+                }
+            }
+
+            cleanedData.Columns.Add("NewReference", typeof(string));
+            cleanedData.Columns.Add("HOD", typeof(string));
+
+            ReferenceParser.ReferenceParser rp = new ReferenceParser.ReferenceParser();
+
+            foreach (DataRow dr in cleanedData.Rows)
+            {
+                bool found = false;
+                foreach (DataColumn dc in cleanedData.Columns)
+                {
+                    if (columnHeaders.Contains(dc.ColumnName))
+                    {
+                        if (found == false)
+                        {
+                            dr["NewReference"] = "";
+                            dr["HOD"] = "";
+                            foreach (string s in rp.Parse(dr[dc.ColumnName].ToString(), true, true))
+                            {
+
+                                if (s.Trim() != "")
+                                { 
+                                    string[] splitRefHod = s.Split(new string[] { "??$??" }, StringSplitOptions.None);
+                                    dr["NewReference"] = splitRefHod[0];
+                                    dr["HOD"] = splitRefHod[1];
+                                    found = true;
+                                    break;
+                                }
+                                else
+                                {
+                                    found = false;
+                                    dr["NewReference"] = "";
+                                    dr["HOD"] = "";
+                                }
+                            }
+                    
+                        }
+                    }
+                }
+            }
+            return cleanedData;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private List<string> columnHeaders;
+    
+        /// <summary>
+        /// Output a CSV file from the DataTable returned from the OutputScannedData Method.
+        /// </summary>
+        /// <param name="outputLocation">Where the file should be output too.</param>
+        /// <param name="fileName">new name for the output file.</param>
+        /// <param name="cleanedData">provide a datatable.</param>
+        private void ReturnReformatDataTableAsCsv(DataTable cleanedData, string outputLocation, string fileName)
+        {
+            New_Wrapper.DataHandler csvHandler = new New_Wrapper.DataHandler();
+            csvHandler.GenerateCSV(cleanedData, outputLocation, fileName, "#");
+        }
+
+        /// <summary>
+        /// Output a Excel workbook for the Datatable returned from OutputScannedata Method.
+        /// </summary>
+        /// <param name="outputLocation">Where the file should be output too.</param>
+        /// <param name="fileName">new name for the output file.</param>
+        /// <param name="cleanedData">provide a datatable.</param>
+        /// <param name="sheetname">The sheet where the data will be exporeted from</param>
+        private void ReturnReformatDataTableAsExcel(DataTable cleanedData, string outputLocation, string fileName, string sheetname = "Sheet1")
+        {
+            cleanedData.ExportToExcel(fileName, outputLocation);
+        }
+
+#endregion
+
+#region public methods
+
+        public List<string> ColumnHeaders
+        {
+            get { return columnHeaders; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fileLocation">Where the file should be output too.</param>
+        /// <param name="fileName">new name for the output file.</param>
+        /// <param name="outputFormat">What data structure the output should be.</param>
+        /// <param name="fileLocation">Where the file should be output too.</param>
+        /// <param name="sheetName">Optional for Excel: The sheet where the data will be exporeted from</param>
+        public void ReturnReferenceScannedData(string fileLocation, OutputFormat outputFormat, List<string> columnHeaders, string outputLocation, string fileName, string sheetName = "Sheet1")
+        {
+        
+            switch(outputFormat)
+            {
+                case OutputFormat.CSV:
+                    ReturnReformatDataTableAsCsv(OutputScannedData(fileLocation, outputFormat, FileType.csv, columnHeaders), outputLocation, fileName);
+                    break;
+                case OutputFormat.Excel:
+                    ReturnReformatDataTableAsExcel(OutputScannedData(fileLocation, outputFormat, FileType.xls, columnHeaders, sheetName), outputLocation, fileName);
+                    break;
+                case OutputFormat.DataTable:
+                    throw new Exception("Use the data table overload ya bampot");
+                default:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Returns a datatable of Data scanned for references.
+        /// </summary>
+        /// <param name="filePath">FilePath to where the excel or csv file is located.</param>
+        /// <param name="outputFormat">What data structure the output should be.</param>
+        /// <param name="inputExtention">File extension of the input file.</param>
+        /// <param name="sheetName">Optional Param for the sheet name if input is of type Excel</param>
+        /// <returns>Returns a datatable</returns>
+        public DataTable ReturnReferenceScannedDataTable(string filePath, OutputFormat outputFormat, FileType inputExtension, List<string> columnHeaders, string sheetName = "Sheet1")
+        {
+            switch(inputExtension)
+	        {   
+                case FileType.csv:
+                    return OutputScannedData(filePath, OutputFormat.DataTable, FileType.csv, columnHeaders);
+                case FileType.xls:
+                    return OutputScannedData(filePath, OutputFormat.DataTable, FileType.xls, columnHeaders, sheetName);
+                case FileType.xlsm:
+                    return OutputScannedData(filePath, OutputFormat.DataTable, FileType.xlsm, columnHeaders, sheetName);
+                case FileType.xlsx:
+                    return OutputScannedData(filePath, OutputFormat.DataTable, FileType.xlsx, columnHeaders, sheetName);
+		        default:
+                    return null;
+	        }
+        }
+
+        
+
+#endregion 
+
+    }
+}
